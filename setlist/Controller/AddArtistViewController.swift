b963a1e0971d09cc63f1ac63a14aca4bfd844a2a
Benoit <bdeguine91@gmail.com>
2016-08-30 22:30:24 +0200

    Modification du nom des fichiers langues.

---
+++ setlist/Controller/AddArtistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddArtistViewController.swift
+//  setlist
+//
+//  Created by Benoit Deguine on 30/08/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class AddArtistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ setlist/Extensions/DimBackground.swift
@@ -0,0 +1,13 @@
+//
+//  DimBackground.swift
+//  setlist
+//
+//  Created by Benoit Deguine on 30/08/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class DimBackground: NSObject {
+
+}
