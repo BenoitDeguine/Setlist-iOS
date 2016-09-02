908a5b872e136ca056f6c6a5252bbb9602b46547
Benoit <bdeguine91@gmail.com>
2016-09-02 16:42:57 +0200

    Ajout de la view qui constituera la cellule d'accueil

---
+++ setlist/Cell/MyArtistCellCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MyArtistCellCollectionViewCell.swift
+//  Setlist
+//
+//  Created by Benoit Deguine on 01/09/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class MyArtistCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail:UIImageView!
+    @IBOutlet weak var name:UILabel!
+    
+    var artist:Artist!
+    
+    func configureCell(artist:Artist){
+        self.artist = artist
+    }
+    
+}
