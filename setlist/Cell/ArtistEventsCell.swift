bcafe7e93198bebd226a59aa4194be5a77366b0c
Benoit <bdeguine91@gmail.com>
2016-09-04 23:24:08 +0200

    Cellule pour afficher la liste des derniers concerts d'un groupe

---
+++ setlist/Cell/ArtistEventsCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtistSetlistsTableViewCell.swift
+//  Setlist
+//
+//  Created by Benoit Deguine on 04/09/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class ArtistEventsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
