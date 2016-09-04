ef652f9967421b40b85f43fabc512b202d258c55
Benoit <bdeguine91@gmail.com>
2016-09-04 10:15:47 +0200

    Ajout des constantes URL

--- setlist/Constant/General.swift
+++ setlist/Constant/General.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 struct App {
-
+    
     struct File {
         static let folderName:String = "artists"
     }
-
+    
+    struct URL {
+        static let spotify:String = "https://api.spotify.com/v1/"
+        static let setlistfm:String = "http://api.setlist.fm/rest/0.1/"
+        static let musicbrainz:String = "http://musicbrainz.org/ws/2/"
+    }
 }
