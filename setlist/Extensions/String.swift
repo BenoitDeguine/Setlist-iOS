00b666031e17157809d9b6f754e76e1b18757472
Benoit <bdeguine91@gmail.com>
2016-09-10 10:54:12 +0200

    Ajout de l'extension à la classe String pour avoir la méthode Trim()

---
+++ setlist/Extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  Setlist
+//
+//  Created by Benoit Deguine on 10/09/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    
+    func trim() -> String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+    
+}
