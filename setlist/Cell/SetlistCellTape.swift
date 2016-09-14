c91fdd1dfca999169d3fc1797f017fe8737df440
Benoit <bdeguine91@gmail.com>
2016-09-14 19:00:38 +0200

    Cellule lorsqu'un titre joué est une musique enregistrée

---
+++ setlist/Cell/SetlistCellTape.swift
@@ -0,0 +1,28 @@
+//
+//  SetlistCellTape.swift
+//  Setlist
+//
+//  Created by Benoit Deguine on 12/09/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class SetlistCellTape: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var info: UILabel!
+    @IBOutlet weak var imageTape: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.imageTape.tintColor = UIColor().tapeSetlistColor()
+        self.title.textColor = UIColor().tapeSetlistColor()
+        self.info.textColor = UIColor().tapeSetlistColor()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
