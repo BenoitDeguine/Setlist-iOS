753aa8cd65147201ce957ada20097d11ebafd0a9
Benoit <bdeguine91@gmail.com>
2016-09-14 19:00:04 +0200

    Header des section de la partie setlist

---
+++ setlist/Cell/SetlistHeaderSectionTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SetlistHeaderSectionTableViewCell.swift
+//  Setlist
+//
+//  Created by Benoit Deguine on 11/09/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class SetlistHeaderSectionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
