e9c142f88a22bf08c66aca322a94185081da83a7
Benoit <bdeguine91@gmail.com>
2016-09-14 19:04:51 +0200

    Si le set est un "Encore"

--- setlist/Model/Set.swift
+++ setlist/Model/Set.swift
@@ -12,13 +12,22 @@ class Set {
     
     var name:String!
     var song = [Song]()
+    var isEncore:Bool = false
     
     init(value:NSDictionary) {
-        
+
         if value.objectForKey("@name") != nil {
             self.name = value.objectForKey("@name") as! String
         } else {
             self.name = ""
+        }
+        
+        // Si le set est un "Encore"
+        if value.objectForKey("@encore") != nil {
+            self.isEncore = true
+            self.name = value.objectForKey("@encore") as! String
+        } else {
+            self.isEncore = false
         }
         
         if (value.objectForKey("song")!.isKindOfClass(NSArray)) {
@@ -30,7 +39,7 @@ class Set {
                 }
             }
         } else {
-            
+            self.song.append(Song(value: value.objectForKey("song") as! NSDictionary))
         }
     }
     
