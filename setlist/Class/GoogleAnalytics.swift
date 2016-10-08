a814fd526af9bc9230ca32c40cc786d14bd89bba
Benoit <bdeguine91@gmail.com>
2016-10-09 00:20:36 +0200

    Pods

---
+++ setlist/Class/GoogleAnalytics.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleAnalytics.swift
+//  Setlist
+//
+//  Created by Benoit Deguine on 09/10/2016.
+//  Copyright © 2016 Benoit Deguine. All rights reserved.
+//
+
+import UIKit
+
+class GoogleAnalytics: NSObject {
+
+}
